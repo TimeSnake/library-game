9ff9bebbd5ddb30a33a30963c837c20e2c2e7b88
funzter <funzter@mail.timesnake.de>
2023-05-18 11:27:34 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/library/game/TmpGameInfo.java
+++ src/main/java/de/timesnake/library/game/TmpGameInfo.java
@@ -7,76 +7,75 @@ package de.timesnake.library.game;
 import de.timesnake.database.util.game.DbGameInfo;
 import de.timesnake.database.util.game.DbTmpGameInfo;
 import de.timesnake.database.util.object.Type;
-
 import java.util.List;
 
 public class TmpGameInfo extends GameInfo {
 
-    private Integer autoStartPlayerNumber;
-    private Integer minPlayerNumber;
-    private List<Integer> teamSizes;
-    private Type.Availability teamMerge;
-    private boolean showSelectedKits;
-    private boolean requireEqualTimeSize;
-    private boolean hideTeams;
-    private Type.Discord discordType;
-    private List<String> description;
+  private Integer autoStartPlayerNumber;
+  private Integer minPlayerNumber;
+  private List<Integer> teamSizes;
+  private Type.Availability teamMerge;
+  private boolean showSelectedKits;
+  private boolean requireEqualTimeSize;
+  private boolean hideTeams;
+  private Type.Discord discordType;
+  private List<String> description;
 
-    public TmpGameInfo(DbTmpGameInfo game) {
-        super(game);
-    }
+  public TmpGameInfo(DbTmpGameInfo game) {
+    super(game);
+  }
 
-    @Override
-    protected void loadFromDatabase(DbGameInfo game) {
-        super.loadFromDatabase(game);
-        DbTmpGameInfo tmpGame = ((DbTmpGameInfo) game);
-        this.autoStartPlayerNumber = tmpGame.getAutoStartPlayerNumber();
-        this.minPlayerNumber = tmpGame.getMinPlayerNumber();
-        this.teamSizes = tmpGame.getTeamSizes();
-        this.teamMerge = tmpGame.getTeamMergeAvailability();
-        this.showSelectedKits = tmpGame.showSelectedKits();
-        this.requireEqualTimeSize = tmpGame.isEqualTeamSizeRequired();
-        this.hideTeams = tmpGame.hideTeams();
-        this.discordType = tmpGame.getDiscordType();
-        if (this.discordType == null) {
-            this.discordType = Type.Discord.FORBIDDEN;
-        }
-        this.description = tmpGame.getDescription();
+  @Override
+  protected void loadFromDatabase(DbGameInfo game) {
+    super.loadFromDatabase(game);
+    DbTmpGameInfo tmpGame = ((DbTmpGameInfo) game);
+    this.autoStartPlayerNumber = tmpGame.getAutoStartPlayerNumber();
+    this.minPlayerNumber = tmpGame.getMinPlayerNumber();
+    this.teamSizes = tmpGame.getTeamSizes();
+    this.teamMerge = tmpGame.getTeamMergeAvailability();
+    this.showSelectedKits = tmpGame.showSelectedKits();
+    this.requireEqualTimeSize = tmpGame.isEqualTeamSizeRequired();
+    this.hideTeams = tmpGame.hideTeams();
+    this.discordType = tmpGame.getDiscordType();
+    if (this.discordType == null) {
+      this.discordType = Type.Discord.FORBIDDEN;
     }
+    this.description = tmpGame.getDescription();
+  }
 
-    public Integer getAutoStartPlayerNumber() {
-        return autoStartPlayerNumber;
-    }
+  public Integer getAutoStartPlayerNumber() {
+    return autoStartPlayerNumber;
+  }
 
-    public Integer getMinPlayerNumber() {
-        return minPlayerNumber;
-    }
+  public Integer getMinPlayerNumber() {
+    return minPlayerNumber;
+  }
 
-    public List<Integer> getTeamSizes() {
-        return teamSizes;
-    }
+  public List<Integer> getTeamSizes() {
+    return teamSizes;
+  }
 
-    public Type.Availability getTeamMerge() {
-        return teamMerge;
-    }
+  public Type.Availability getTeamMerge() {
+    return teamMerge;
+  }
 
-    public boolean showSelectedKits() {
-        return showSelectedKits;
-    }
+  public boolean showSelectedKits() {
+    return showSelectedKits;
+  }
 
-    public boolean isEqualTimeSizeRequired() {
-        return requireEqualTimeSize;
-    }
+  public boolean isEqualTimeSizeRequired() {
+    return requireEqualTimeSize;
+  }
 
-    public boolean hideTeams() {
-        return hideTeams;
-    }
+  public boolean hideTeams() {
+    return hideTeams;
+  }
 
-    public Type.Discord getDiscordType() {
-        return discordType;
-    }
+  public Type.Discord getDiscordType() {
+    return discordType;
+  }
 
-    public List<String> getDescription() {
-        return description;
-    }
+  public List<String> getDescription() {
+    return description;
+  }
 }
